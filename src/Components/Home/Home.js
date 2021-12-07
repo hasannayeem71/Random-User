017df5ca80609214044fce28b60ffa2c96791053
Mahmudul Hasan Nayeem <2052cse00867@manarat.ac.bd>
2021-12-07 22:13:03 +0600

    added sort system

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,6 +6,8 @@ import styles from './Home.module.css';
 
 const Home = () => {
     const [users,setUsers] = React.useState([]);
+    const [assending,setAssending] = React.useState(false);
+    
     const [seed,setSeed] = React.useState('');
     const [loading,setLoading] = React.useState(true);
     useEffect(()=>{
@@ -14,9 +16,51 @@ const Home = () => {
             setUsers(res.data.results);
             setSeed(res.data.info.seed);
             setLoading(false);
-           
+                // setUsers(res.data.results);  
         })
-    },[])
+    },[]
+    );
+
+    console.log(users);
+    const handleSort = (users)=>{
+        users.sort(function(a, b) {
+          var nameA = a.location.country.toUpperCase(); // ignore upper and lowercase
+          var nameB = b.location.country.toUpperCase(); // ignore upper and lowercase
+        
+            if(!assending){
+                setAssending(true);
+                if (nameA < nameB) { 
+                    return -1;
+                  }
+                  if (nameA > nameB) {  
+                    return 1;
+                  }
+                  // names must be equal
+                  return 0;
+              
+            }
+            else{
+                setAssending(false);
+                if (nameA < nameB) { 
+                    return 1;
+                  }
+                  if (nameA > nameB) {  
+                    return -1;
+                  }
+                  // names must be equal
+
+                  return 0;
+            }
+        });
+        
+     
+
+        const newUser= [...users];
+        setUsers(newUser);
+       
+            
+    }
+
 
     if(loading){
         return <PreLoader/>
@@ -25,11 +69,16 @@ const Home = () => {
     }
     return (
         <div className="App">
+             <button className={styles.sortButton} onClick={()=>handleSort(users )}>Sort by country</button>
         <div className={styles.userContainer}>
             {
-                users?.map((user,index)=><User key={index} index={index} user={user} seed={seed}/>)
+                users.map((user,index)=><User key={index} index={index} user={user} seed={seed}/>)
             }
-        </div></div>
+        </div>
+        {/* button to sort by country */}
+     
+      </div>
+       
     );
 };
 
--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './User.module.css';
 const User = ({user,seed,index}) => {
-    const {name,picture} = user;
+    const {name,picture,location} = user;
     const navigate = useNavigate();
     const handleClick = () => {
         navigate(`/details`,{ state: {seed,index} });
@@ -12,6 +12,7 @@ const User = ({user,seed,index}) => {
         <div className={styles.userContainer}>
             <img src={picture.large} alt='' className={styles.profileImage}/>
             <p>{name.title} {name.first} {name.last}</p>
+            <p>From : {location.country}</p>
             <button className={styles.detailsButton}
             onClick={handleClick}
             >DETAILS</button>
